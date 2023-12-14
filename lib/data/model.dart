8162a220c3ecce4afcaf5cc154b8491e78c9e59e
adiljaz <146193187+adiljaz@users.noreply.github.com>
2023-12-14 09:50:33 +0530

    .

--- lib/data/model.dart
+++ lib/data/model.dart
@@ -1,12 +1,10 @@
-
 import 'package:hive/hive.dart';
 part 'model.g.dart';
 
 @HiveType(typeId: 1)
 class Cameramodel {
-
   @HiveField(0)
   String camera;
 
- Cameramodel({required this.camera});
+  Cameramodel({required this.camera});
 }
